0597a77d9cc79dfe3ab10935e2b70ae45eb7d955
Martin <martin@luckycloud.de>
2020-04-19 21:59:31 +0200

    Update select.h
    Works correctly

--- src/select.h
+++ src/select.h
@@ -11,7 +11,12 @@
 void Display(void);
 void Display_clear(void);
 void Display_String(int width, int Heigh, int TextX, int TextY, const char* text, sFONT* font, int areaX,int aeraY );
-#if defined ( USE_213_BWSoft ) || defined ( USE_583_BW ) || defined ( USE_583_BWR ) || \
+#if defined ( USE_213_BWR ) || defined ( USE_213_BWY ) || defined ( USE_154_BWY ) || \
+    defined ( USE_154_BWR_152 )  || defined ( USE_260_BWR ) || \
+    defined ( USE_270_BWR ) || defined ( USE_290_BWR ) || defined ( USE_290_BWY ) || \
+    defined ( USE_420_BWR ) || defined ( USE_420_BWY )
+void Display_picture(const unsigned char* IMAGE_BLACK,const unsigned char* IMAGE_RED);
+#elif defined ( USE_213_BWSoft ) || defined ( USE_583_BW ) || defined ( USE_583_BWR ) || \
         defined ( USE_583_BWY ) || defined ( USE_750_BWR ) || defined ( USE_750_BWY ) || \
         defined ( USE_750_BW  ) || defined ( USE_270_BW ) || defined ( USE_290_BWSoft ) || \
         defined ( USE_420_BW ) || defined ( USE_213_BW ) || defined ( USE_260_BW )
@@ -22,12 +27,4 @@ void Display_picture(const unsigned char* IMAGE_BLACK);
 
 #endif /* SELECT FILE END */ 
 
-// NOTE This #if conditional was originally flawed and did not let me select USE_154_BW_GREEN
-/*
-defined ( USE_213_BWR ) || defined ( USE_213_BWY ) || defined ( USE_154_BWY ) || \
-    defined ( USE_154_BWR_152 )  || defined ( USE_260_BWR ) || \
-    defined ( USE_270_BWR ) || defined ( USE_290_BWR ) || defined ( USE_290_BWY ) || \
-    defined ( USE_420_BWR ) || defined ( USE_420_BWY )
-void Display_picture(const unsigned char* IMAGE_BLACK,const unsigned char* IMAGE_RED);
-#elif
-*/
+
